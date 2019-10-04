d543f5e113686bb7be2032c2a299e8920d81cabc
manAbl <mblanco@agrolevels.com>
2019-10-04 15:11:15 -0500

    initial commit with one third of the second section of the course

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,32 @@
+const k = 3
+const outputs = []
+const predictionPoint = 300
+const distance = point => Math.abs(point - predictionPoint)
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+    outputs.push([dropPosition, bounciness, size, bucketLabel])
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+    const bucket = _.chain(outputs)
+        .map(row => [distance(row[0]), row[3]])
+        .sortBy(row => row[0])
+        .slice(0, k)
+        .countBy(row => row[1])
+        .toPairs()
+        .sortBy(row => row[1])
+        .last()
+        .first()
+        .parseInt()
+        .value()
+
+    console.info(`Your bucket will fall more probably into: ${bucket}`)
 }
 
+function distance(point) {
+    return Math.abs(point - predictionPoint)
+}
+
+function splitDataSet(data, testCount) {
+
+}
